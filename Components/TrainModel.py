bb0c768aaf26cbac01ed670078cb592faa3bcf4d
taltmann0818 <42226531+taltmann0818@users.noreply.github.com>
2025-03-31 17:30:45 -0400

    Update TrainModel.py

--- Components/TrainModel.py
+++ Components/TrainModel.py
@@ -12,8 +12,6 @@ import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
 
-import shap
-
 from torch.utils.data import Dataset, DataLoader
 from sklearn.preprocessing import StandardScaler
 
