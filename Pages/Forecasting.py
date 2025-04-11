013daf098a68bd30362834decda90380ad361ec0
taltmann0818 <taltmann0818@gmail.com>
2025-04-11 10:12:44 -0400

    Refactor forecasting page with improved input handling.
    Updated mode selection logic, added options toggle, and streamlined input validation for better usability. Enhanced layout by restructuring forecast-related sections and ensuring alignment with user-provided inputs.

--- Pages/Forecasting.py
+++ Pages/Forecasting.py
@@ -5,6 +5,7 @@ import plotly.express as px
 from plotly.subplots import make_subplots
 import datetime
 import numpy as np
+from pathlib import Path
 
 # Custom libraries
 from Components.TrainModel import TEMPUS, DataModule
@@ -35,79 +36,77 @@ if st.experimental_user.is_logged_in:
             submit = st.button("Forecast")
 
             # Segmented control to toggle showing the ticker input
-            selection = st.segmented_control("Mode", options=["val_1", "val_2"], selection_mode="single")
+            modes = ["Scan", "Single", "Multi"]
+            mode_selection = st.segmented_control(
+                "Mode", modes, selection_mode="single", help="This is the mode used for price forecasting."
+            )
 
             # Display ticker input conditionally based on selection
-            if selection == "val_1":
+            if mode_selection == "Single":
                 ticker_select = st.text_input("Ticker")
+            if mode_selection == "Scan":
+                ticker_select = st.selectbox("Index",['NASDAQ','S&P500','RUSSELL2000','DOWJONES'])
             else:
                 ticker_select = None
 
+            options_toggle = st.toggle("Include options?")
+
             st.subheader("Settings")
-            today = datetime.datetime.now()
-            data_range = st.slider(
-                "Data Range",
-                value=(datetime.date(2000, 1, 1), today.date()),
-                format="MM/DD/YYYY"
-            )
-
-            directory_path = 'Models/'
-            models = [file.name for file in Path(directory_path).glob('*.pt') if file.is_file()]
+            models = [file.name for file in Path('Models/').glob('*.pt') if file.is_file()]
             model_select = st.selectbox('Select Model', models, help="This is the model used for price prediction.")
-
             prediction_window = st.slider("Prediction Window", 1, 10, 5)
-            sequence_window = st.slider("LSTM Sequence Window", 1, 200, 50)
+            sequence_window = st.slider("LTSM Sequence Window", 1, 200, 50)
 
             st.write("Backtesting Parameters")
-            initial_capital = st.number_input("Initial Capital", value=1000)
-            pct_change_entry = st.number_input(
-                "% Change for BUY",
-                help="This measures the relative gain between an equity's actual price and the predicted price for a BUY to be signaled.",
-                value=5.00
-            )
-            pct_change_exit = st.number_input(
-                "% Change for SELL",
-                help="This measures the relative decrease between an equity's actual price and the predicted price for a SELL to be signaled.",
-                value=2.00
-            )
-            risk_free_rate = st.number_input("Risk Free Rate (%)", value=0.25)
+            pct_change_entry = st.number_input("% Change for BUY",
+                                               help="This measures the relative gain between an equity's actual price and the predicted price for a BUY to be signalled.",
+                                               value=5.00)
+            pct_change_exit = st.number_input("% Change for SELL",
+                                              help="This measures the relative decrease between an equity's actual price and the predicted price for a SELL to be signalled.",
+                                              value=2.00)
 
     with col1:
         st.subheader("Forecasting")
         if submit:
             missing_fields = []
-
-            if selection == "val_1" and not ticker_select:
+            if (mode_selection == "Scan" or mode_selection == "Single") and not ticker_select:
                 missing_fields.append("Ticker")
-
-            if not models:
+            if not model_select:
                 missing_fields.append("Model")
+            if not prediction_window:
+                missing_fields.append("Prediction Window")
+            if not sequence_window:
+                missing_fields.append("LTSM Sequence Window")
+            if not pct_change_entry:
+                missing_fields.append("% Change for BUY")
+            if not pct_change_exit:
+                missing_fields.append("% Change for SELL")
 
             # Check if any fields are missing
             if missing_fields:
                 st.error(f"Please fill out the following fields: {', '.join(missing_fields)}")
+
             else:
                 # Place your backtesting function call or logic here
                 st.success("Running backtest with the provided inputs...")
 
+                with st.container(border=True):
+                    st.subheader("Train History")
 
-        with st.container(border=True):
-            st.subheader("Train History")
-            
-            st.bar_chart(np.random.randn(50, 3))
+                    st.bar_chart(np.random.randn(50, 3))
 
-        with st.container(border=True):
-            st.subheader("Predictions")
-            
-            st.bar_chart(np.random.randn(50, 3))
-        
-        with st.container(border=True):
-            st.subheader("Metrics")
-            
-            mcol1, mcol2, mcol3 = st.columns(3)
-            mcol1.metric("Loss", "70")
-            mcol2.metric("MSE", "9")
-            mcol3.metric("MAPE", "5%")
+                with st.container(border=True):
+                    st.subheader("Predictions")
+
+                    st.bar_chart(np.random.randn(50, 3))
+
+                with st.container(border=True):
+                    st.subheader("Metrics")
+
+                    mcol1, mcol2, mcol3 = st.columns(3)
+                    mcol1.metric("Loss", "70")
+                    mcol2.metric("MSE", "9")
+                    mcol3.metric("MAPE", "5%")
 
        
 # Sidebar: Settings
