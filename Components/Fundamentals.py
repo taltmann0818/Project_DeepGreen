bc12ff377455c14446895a3ae48b0fe72257aecf
taltmann0818 <42226531+taltmann0818@users.noreply.github.com>
2025-05-13 15:18:49 -0400

    Rollback of some changes

--- Components/Fundamentals.py
+++ Components/Fundamentals.py
@@ -133,12 +133,14 @@ class FundementalData:
             date_np = np.datetime64(asof)
         else:
             date_np = np.datetime64(datetime.now().strftime("%Y-%m-%d"))
+        busday_np = str(np.busday_offset(date_np,offsets=0,roll='backward',holidays=generate_us_market_holidays(2020,2030)))
+        date_str = str(busday_np)
         try:
-            busday_str = str(np.busday_offset(date_np,offsets=0,roll='backward',holidays=generate_us_market_holidays(2020,2030)))
-            resp = self.client.get_daily_open_close_agg(ticker, date=busday_str)
-            return getattr(resp, "close", None) if getattr(resp, "close", None) is not None else getattr(resp, "open", None)
+            resp = self.client.get_daily_open_close_agg(ticker, date=date_str)
+            close = getattr(resp, "close", None)
+            return close if close is not None else getattr(resp, "open", None)
         except Exception as err:
-            logging.warning(f"[get_close_price] {ticker} failed for {asof} and/or {busday_str}: {err}")
+            logging.warning(f"[get_close_price] {ticker} failed for {asof}: {err}")
             return pd.DataFrame()
     
     def get_fundamentals(self):
